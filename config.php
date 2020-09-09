1b8a1801e8973f9e488b9fc3ae26d8f0224018b0
Abdul Aldukhn <abbcc123123@gmail.com>
2020-09-09 16:57:31 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,23 @@
-<?php
-    $host = "sql312.epizy.com";
-    $userName = "epiz_26495741";
-    $password = "M7Xghh6u3XT";
-    $dbName = "epiz_26495741_emails";
-    // Create database connection
-    $conn = new mysqli($host, $userName, $password, $dbName);
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
+<?php
+
+    $host = "HOSTNAME";
+
+    $userName = "USERNAME";
+
+    $password = "DATABASEPASS";
+
+    $dbName = "DBNAME";
+
+    // Create database connection
+
+    $conn = new mysqli($host, $userName, $password, $dbName);
+
+    // Check connection
+
+    if ($conn->connect_error) {
+
+    die("Connection failed: " . $conn->connect_error);
+
+    }
+
     ?>
