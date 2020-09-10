8155c473736fad16cea1caf9e46ee7528d681096
Abdul Aldukhn <abbcc123123@gmail.com>
2020-09-10 10:11:41 +0300

    Update displayprofile.php

--- displayprofile.php
+++ displayprofile.php
@@ -1,31 +1,62 @@
-<?php
-//host
-$host = "sql312.epizy.com";
-//user name
-$username = "epiz_26495741";
-//database password
-$pwd = "M7Xghh6u3XT";
-//database name.
-$db = "epiz_26495741_emails";
-
-$con=mysqli_connect($host,$username,$pwd,$db) or die("Unable to Connect");
-
-if(mysqli_connect_error($con))
-{
-	echo "Failed to connect";
-}
-
-$query=mysqli_query($con,"SELECT * FROM contact_form_info");
-
-if($query)
-{
-while($row=mysqli_fetch_array($query))
-	{
-	$flag[]=$row;
-	}
-print(json_encode($flag));
-}
-mysqli_close($con);
-?>
-
-
+<?php
+
+//الهوست
+
+$host = "RRRRR.com";
+
+//إسم المستخدم
+
+$username = "RRRRRRR";
+
+//كلمة مرور قاعدة البيانات
+
+$pwd = "RRRRRRR";
+
+//قاعدة البيانات.
+
+$db = "RRRRRR";
+
+
+
+$con=mysqli_connect($host,$username,$pwd,$db) or die("Unable to Connect");
+
+
+
+if(mysqli_connect_error($con))
+
+{
+
+	echo "Failed to connect";
+
+}
+
+
+
+$query=mysqli_query($con,"SELECT * FROM contact_form_info");
+
+
+
+if($query)
+
+{
+
+while($row=mysqli_fetch_array($query))
+
+	{
+
+	$flag[]=$row;
+
+	}
+
+print(json_encode($flag));
+
+}
+
+mysqli_close($con);
+
+?>
+
+
+
+
+
